859c374e3cf55349bcc29c5ea570c8da3a197455
Camille <camille.sayous@epitech.eu>
2022-05-01 16:10:39 +0200

    [ADD] app

---
+++ chat/config/test.exs
@@ -0,0 +1,18 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chat, ChatWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "fzZlBO7ItzWxBzn9qWxWmyuC78ic13yyKfnSeUFcKIYx6XdL8Han6h317/6DM3wM",
+  server: false
+
+# In test we don't send emails.
+config :chat, Chat.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
